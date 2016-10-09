aec68b74195d78f4ff938292441a9535fb07703c
yu cheng <yucheng11235@163.com>
2016-10-09 22:08:40 +0800

    add ep28

---
+++ ep28.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+#to print the true or false or the input expression
+x = input("Input your expression:\n")
+if(eval(x)):
+    print("The answer is True")
+else:
+    print("The answer is Fasle")
