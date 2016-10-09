849b7b232c91478f97e50a47711f99065d4a1599
yu cheng <yucheng11235@163.com>
2016-10-09 22:14:32 +0800

    update ep 28

--- ep28.py
+++ ep28.py
@@ -1,7 +1,9 @@
 # -*- coding:utf-8 -*-
 #to print the true or false or the input expression
 x = input("Input your expression:\n")
-if(eval(x)):
-    print("The answer is True")
-else:
-    print("The answer is Fasle")
+while(x!="q"):
+    if(eval(x)):
+        print("The answer is True")
+    else:
+        print("The answer is Fasle")
+    x = input("Input your expression:\n")
